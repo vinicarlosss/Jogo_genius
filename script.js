49a7ecd923f768a82742d67919e63752c8e0eef2
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2021-09-27 09:23:26 -0300

    adicionadas funções de cor e tempo para aparecerem as cores

---
+++ script.js
@@ -0,0 +1,34 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+//0 - verde
+//1 - vermelho
+//2 - amarelo
+//3 - azul
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i in order){
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+}
+
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');  
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    });
+}
