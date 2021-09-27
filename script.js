e4a7cbc9e55e70a315702da2fc106b0c2b569ce0
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2021-09-27 09:39:39 -0300

    adicionado função de clique do usuario

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
+//criar ordem aleatória de cores
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
     order[order.length] = colorOrder;
@@ -23,6 +24,7 @@ let shuffleOrder = () => {
     }
 }
 
+//acende a próxima cor
 let lightColor = (element, number) => {
     number = number * 500;
     setTimeout(() => {
@@ -32,3 +34,30 @@ let lightColor = (element, number) => {
         element.classList.remove('selected');
     });
 }
+
+//checa se os botões clicados são a mesma ordem da gerada no jogo
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i]!= order[i]){
+            lose();
+            break;
+        };
+    };
+    if(clickedOrder.length == order.length){
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível`);
+        nextLevel();
+    };
+}
+
+//função para o clique do usuario
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+    });
+
+    checkOrder();
+}
+
