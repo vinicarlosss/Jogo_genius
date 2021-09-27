44fd9a76181ba4281f0491876dbe306a90bf35f1
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2021-09-27 09:58:20 -0300

    adicionadas as funções de proximo nivel, game over e play game

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ let order = [];
 let clickedOrder = [];
 let score = 0;
 
-//0 - verde
-//1 - vermelho
-//2 - amarelo
-//3 - azul
-
 const blue = document.querySelector('.blue');
 const red = document.querySelector('.red');
 const green = document.querySelector('.green');
@@ -56,8 +51,48 @@ let click = (color) => {
 
     setTimeout(() => {
         createColorElement(color).classList.remove('selected');
-    });
-
-    checkOrder();
+        checkOrder();
+    }, 250);
 }
 
+//função que retorna a cor
+let createColorElement = (color)  => {
+    if(color == 0){
+        return green;
+    }else if(color == 1){
+        return red;
+    }else if (color == 2){
+        return yellow;
+    }else if (color == 3){
+        return blue;
+    }
+}
+
+//função para próximo nível do jogo
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+//função para game over
+let gameOver = () => {
+    alert(`Pontuação: ${score}!\nVocê perdeu o jogo\nClique em OK para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+
+    playGame();
+}
+
+let playGame = () => {
+    alert('Bem Vindo ao Gênesis! Iniciando novo jogo!');
+    score = 0;
+    
+    nextLevel();
+}
+
+green.addEventListener('click', click(0));
+red.addEventListener('click', click(1));
+yellow.addEventListener('click', click(2));
+blue.addEventListener('click', click(3));
+
+playGame();
